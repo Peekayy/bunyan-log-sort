0c4e0994ea72bd2c364ff8f016d47b4527ec63d4
Gaël Bouganne <gael@teractys.fr>
2021-06-29 16:07:59 +0200

    Version 1.1.0

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # bunyan-log-sort
 
 Sorts bunyan formatted logs according to their `time` field. This way you can easily concat various bunyan log files and still get the result ordered.
+Also wraps non bunyan lines into interpolated bunyan logs and attempts to merge bunyan lines that were split into multiple lines (by journalctl for instance). 
 
 ## Installation
 
--- bunyan-sort.js
+++ bunyan-sort.js
@@ -10,29 +10,83 @@ stdin.resume();
 stdin.setEncoding('utf8');
 
 stdin.on('data', function (chunk) {
-    inputChunks.push(chunk);
+    if (chunk.length) {
+        inputChunks.push(chunk);
+    }
 });
 
-stdin.on('end', function () {
-    const inputData = inputChunks.join().split(/\r\n|\n/);
-    const validLines = [];
-    for (let line of inputData) {
-        try {
-            line = JSON.parse(line, (k, v) => {
-                if (k === 'time') {
-                    return new Date(v);
-                } else {
-                    return v;
-                }
+function wrapIntoBunyan(data, time) {
+    return {
+        name: "bunyan-log-sort-wrapper",
+        hostname: "???",
+        pid: "???",
+        level: 20,
+        msg: data,
+        time,
+        v: 0
+    }
+}
 
-            });
-            if (line.time) {
+function reviver(k, v) {
+    if (k === 'time') {
+        return new Date(v);
+    } else {
+        return v;
+    }
+
+}
+
+stdin.on('end', function () {
+    const inputData = inputChunks.join("").split(/\r\n|\n/);
+    const validLines = [];
+    let previousTs = new Date(0);
+    let partialJson = [];
+    for (let line of inputData) {
+        if (line) {
+            try {
+                line = JSON.parse(line, reviver);
+                if (!line.time) {
+                    line.time = new Date(previousTs.getTime() + 1);
+                }
+                previousTs = line.time;
                 validLines.push(line);
+            } catch (e) {
+                // Line not parseable, is it split or just arbitrary data ?
+                if (line.startsWith('{"name":') || partialJson.length) {
+                    partialJson.push(line)
+                    if (partialJson.length > 1) {
+                        try {
+                            line = JSON.parse(partialJson.join(""), reviver);
+                            validLines.push(line);
+                            partialJson.length = 0;
+                        } catch (err) {
+                        }
+                    }
+                } else {
+                    // Wrap arbitrary data into bunyan format
+                    const time = new Date(previousTs.getTime() + 1);
+                    validLines.push(wrapIntoBunyan(line, time));
+                    previousTs = time;
+                }
             }
-        } catch (e) {
-            //console.error(line);
         }
     }
+
+    if (partialJson.length) {
+        for (const invalidJson of partialJson) {
+            // Wrap remaining arbitrary data into bunyan format
+            const time = new Date(previousTs.getTime() + 1);
+            validLines.push(wrapIntoBunyan(invalidJson, time));
+            previousTs = time;
+        }
+    }
+
+    for (const l of validLines) {
+        if (!l.time.getTime) {
+            console.log(l);
+        }
+    }
+
     validLines.sort((a, b) => {
         if (reverse) {
             return b.time.getTime() - a.time.getTime();
